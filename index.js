aaeab1c00440663b1f7251f0de3524fece560b33
Linux <rizki.st3004@gmail.com>
2020-12-16 19:25:30 +0700

    sesuai soal, request blindspot comment di email aja

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // write your code here
+let request = process.argv[2]
+let parameter = process.argv.slice(3)
+let gameController = require ('./controller/gameController')
+
+
+switch(request) {
+    case 'help':
+        gameController.commandHelp()
+    break;
+    case 'all':
+        gameController.CommandAll()
+    break;
+    case 'start':
+        gameController.startGame()
+    break;
+    case 'end':
+        gameController.endGame()
+    break;
+    case 'add':
+        gameController.commandAdd(parameter[0], parameter[1])
+    break;
+    case 'delete':
+        gameController.commandDelete(parameter[0])
+    break;
+    case 'change':
+        gameController.commandChange(parameter[0], parameter[1])
+    break;
+    case 'levelup':
+        gameController.commandLevelUp(parameter[0], parameter[1])
+    break;
+    default:
+
+}
